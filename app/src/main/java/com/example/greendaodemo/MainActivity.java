685487ccbfce072fc155faedb735c2f2d60258a5
cag2050 <cag2050@163.com>
2021-06-18 01:00:09 +0800

    run demo success

--- app/src/main/java/com/example/greendaodemo/MainActivity.java
+++ app/src/main/java/com/example/greendaodemo/MainActivity.java
@@ -1,14 +1,121 @@
 package com.example.greendaodemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.greenrobot.greendao.query.Query;
+
+import java.util.List;
+import java.text.DateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+    private View addNoteButton;
+
+    private NoteDao noteDao;
+    private Query<Note> notesQuery;
+    private NotesAdapter notesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpViews();
+
+        // get the note DAO
+        DaoSession daoSession = ((App) getApplication()).getDaoSession();
+        noteDao = daoSession.getNoteDao();
+
+        // query all notes, sorted a-z by their text
+        notesQuery = noteDao.queryBuilder().orderAsc(NoteDao.Properties.Text).build();
+        updateNotes();
     }
+
+    private void updateNotes() {
+        List<Note> notes = notesQuery.list();
+        notesAdapter.setNotes(notes);
+    }
+
+    protected void setUpViews() {
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewNotes);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        notesAdapter = new NotesAdapter(noteClickListener);
+        recyclerView.setAdapter(notesAdapter);
+
+        addNoteButton = findViewById(R.id.buttonAdd);
+        addNoteButton.setEnabled(false);
+
+        editText = findViewById(R.id.editTextNote);
+        editText.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                addNote();
+                return true;
+            }
+            return false;
+        });
+        editText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                boolean enable = s.length() != 0;
+                addNoteButton.setEnabled(enable);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    public void onAddButtonClick(View view) {
+        addNote();
+    }
+
+    private void addNote() {
+        String noteText = editText.getText().toString();
+        editText.setText("");
+
+        final DateFormat df = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM);
+        String comment = "Added on " + df.format(new Date());
+
+        Note note = new Note();
+        note.setText(noteText);
+        note.setComment(comment);
+        note.setDate(new Date());
+        note.setType(NoteType.TEXT);
+        noteDao.insert(note);
+        Log.d("DaoExample", "Inserted new note, ID: " + note.getId());
+
+        updateNotes();
+    }
+
+    NotesAdapter.NoteClickListener noteClickListener = new NotesAdapter.NoteClickListener() {
+        @Override
+        public void onNoteClick(int position) {
+            Note note = notesAdapter.getNote(position);
+            Long noteId = note.getId();
+
+            noteDao.deleteByKey(noteId);
+            Log.d("DaoExample", "Deleted note, ID: " + noteId);
+
+            updateNotes();
+        }
+    };
 }
---
+++ app/src/main/java/com/example/greendaodemo/NoteType.java
@@ -0,0 +1,5 @@
+package com.example.greendaodemo;
+
+public enum NoteType {
+    TEXT, LIST, PICTURE
+}
